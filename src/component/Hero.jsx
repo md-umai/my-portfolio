31f4a0941e58220e51e9e784894eb94fa525558b
md-umai <mdumair786114@gmail.com>
2026-02-22 22:26:24 +0530

    Updates text

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react"
 function Hero() {
   return (
     <section style={{ padding: "80px 20px", textAlign: "center" }}>
-      <h1>Mohammed Umair</h1>
+      <h1 className="bg-red-500">Mohammed Umairrrr</h1>
       <p>Frontend Developer | React | Vite</p>
     </section>
   )
